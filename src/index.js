3546f6f0ab19f462fedfaabb5ddeb01b5a54cfa0
skadyeyes <skadyeyes@skadyeyesdeMacBook-Pro.local>
2018-05-28 15:59:35 +0800

    [todolist]

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ControlPanel from './flux/views/ControlPanel';
-import './index.css';
+import {Provider} from 'react-redux';
+import TodoApp from './todoList/TodoApp';
+
+import store from './todoList/Store.js';
 
 ReactDOM.render(
-  <ControlPanel/>,
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/todoList/Store.js
@@ -0,0 +1,20 @@
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+
+import {reducer as todoReducer} from './todos';
+import {reducer as filterReducer} from './filter';
+
+const win = window;
+
+const reducer = combineReducers({
+  todos: todoReducer,
+  filter: filterReducer
+});
+
+const middlewares = [];
+
+const storeEnhancers = compose(
+  applyMiddleware(...middlewares),
+  (win && win.devToolsExtension) ? win.devToolsExtension() : (f) => f,
+);
+
+export default createStore(reducer, {}, storeEnhancers);

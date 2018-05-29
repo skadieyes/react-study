95d8e97ffe9fa756427f8e0f53dae0dda60110ff
skadyeyes <skadyeyes@skadyeyesdeMacBook-Pro.local>
2018-05-29 11:18:15 +0800

    [update]fetch

--- src/weather_react/index.js
+++ src/weather_react/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Weather from './weather_react/Weather';
+import Weather from './Weather';
 import './index.css';
 
 ReactDOM.render(
